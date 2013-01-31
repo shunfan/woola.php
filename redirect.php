d97e782a077d64423738950df81639963eb0ff90
Perry Du <Perry@Perrys-MacBook-Pro.local>
2013-01-31 22:27:04 +0800

    Add comments

--- redirect.php
+++ redirect.php
@@ -5,16 +5,20 @@ require("config.php");
 $slug = $_GET["slug"];
 $result = mysql_query("SELECT * FROM redirect WHERE short_url='$slug'");
 
+// Find the original link through the database
 while($row = mysql_fetch_array($result)){
 	$original_url = $row['original_url'];
 }
 
+// If the original link doesn't exist, go index
 if(!$original_url){
 	header("location:/");
 }
 
+// Record the hint
 mysql_query("UPDATE redirect SET hits = hits + 1 WHERE short_url = '$slug'");
 
+// redirect to the original link
 echo '<script>window.location.href="' . $original_url . '";</script>';
 
 ?>
